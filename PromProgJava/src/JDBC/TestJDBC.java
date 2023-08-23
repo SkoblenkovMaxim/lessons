fe23ef8713c7d3e484776c31d3c3563facd75172
Maks <goofy.net@yandex.ru>
2023-08-23 23:00:46 +0400

    Maks test comment Maks test JDBC Maks 15.08 - TestJDBC с доработанный Maks 23.08 - TestJDBC рабочий

--- PromProgJava/src/JDBC/TestJDBC.java
+++ PromProgJava/src/JDBC/TestJDBC.java
@@ -3,19 +3,18 @@ package src.JDBC;
 import java.sql.*;
 
 public class TestJDBC{
-    public static void main(String[] args) {
-        String url = "jdbc:postgresql://127.0.0.1:5432/testWork";
-        String user = "goofy";
-        String password = "9GoFuckYourself&";
+    public static void main(String[] args) throws ClassNotFoundException {
+        String url = "jdbc:postgresql://127.0.0.1:5432/work";
+        String user = "postgres";
+        String password = "9Luck_Maks!_%";
         Connection connection;
+        //Загружаем драйвер
+        Class.forName("org.postgresql.Driver");
         try{
-            //Загружаем драйвер
-            String driverName = "org.postgresql.Driver";
-            Class.forName(driverName);
             //Устанавливаем соединение с сервером БД
             connection = DriverManager.getConnection(url, user, password);
             Statement statement = connection.createStatement();
-            System.out.println("БД до внесения записей");
+            System.out.println("1_БД до внесения записей");
             //Выполним запрос в БД
             ResultSet result1 = statement.executeQuery("SELECT * FROM Employee");
             //Выведем результат result1
@@ -23,18 +22,18 @@ public class TestJDBC{
                 System.out.println("UserID: " + result1.getInt("userID") + " userName: " + result1.getString("userName") + " age " + result1.getInt("age"));
             }
             //Вставим значения в таблицу Employee
-            statement.executeUpdate("INSERT INTO Employee (userID, userName, age) VALUES (1, 'Скобленков Максим', 37)");
-            statement.executeUpdate("INSERT INTO Employee (userID, userName, age) VALUES (2, 'Ветлугин Андрей', 37)");
-            System.out.println("БД после добавления записей");
+            statement.executeUpdate("INSERT INTO Employee (userid, username, age, city) VALUES (2, 'Максим', 37, 'Samara')");
+            statement.executeUpdate("INSERT INTO Employee (userid, username, age, city) VALUES (3, 'Андрей', 37, 'Samara')");
+            System.out.println("2_БД после добавления записей");
             result1 = statement.executeQuery("SELECT * FROM Employee");
             while (result1.next()){
-                System.out.println("UserID: " + result1.getInt("userID") + " userName: " + result1.getString("userName") + " age " + result1.getInt("age"));
+                System.out.println("UserID: " + result1.getInt("userID") + " userName: " + result1.getString("userName") + " age " + result1.getInt("age") + " city: " + result1.getString("city"));
             }
-            statement.executeUpdate("DELETE FROM Employee WHERE userID = 2");
-            System.out.println("БД после удаления записи");
+            statement.executeUpdate("DELETE FROM Employee WHERE userID = 3");
+            System.out.println("3_БД после удаления записи");
             result1 = statement.executeQuery("SELECT * FROM Employee");
             while (result1.next()){
-                System.out.println("UserID: " + result1.getInt("userID") + " userName: " + result1.getString("userName") + " age " + result1.getInt("age"));
+                System.out.println("UserID: " + result1.getInt("userID") + " userName: " + result1.getString("userName") + " age " + result1.getInt("age") + " city: " + result1.getString("city"));
             }
         } catch (Exception e){
             e.printStackTrace();
